8acc41e4687f1a2ed1c9a61000f60a0045b59e21
ArizkyKelanaHaryadi <arizky.wb.tk2@gmail.com>
2024-10-05 02:44:24 +0700

    build commit

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -36,7 +36,7 @@ const TextSection = () => {
                 <div className="relative">
                     <a href="/login">
                         <button className="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                            Klik Disini
+                            Klik Disini !
                         </button>
                     </a>
                 </div>
